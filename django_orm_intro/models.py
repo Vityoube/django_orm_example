9e1cba24025ba8eec8cc0b41c6dd3c540a00d427
Viktor Kalashnykov <viktor.kalashnykov@infinitedata.com>
2026-07-12 14:11:38 +0300

    Added validation in models.User

--- django_orm_intro/models.py
+++ django_orm_intro/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,14 @@ class User(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    age = models.IntegerField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    bio = models.TextField(null=True, blank=True)
+
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+    def clean(self):
+        if self.age is not None and self.age < 0:
+            raise ValidationError("Age must be a positive number.")
+
